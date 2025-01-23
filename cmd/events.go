19c73fbeafd6fd8df22f633e325b0002a01710e4
patrick-ivann origo <patrickivann.origo@viacesi.fr>
2025-01-23 10:57:50 +0100

    :sparkles: Add root events command

--- cmd/events.go
+++ cmd/events.go
@@ -1,1 +1,16 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// eventsCmd represents the metrics command
+var eventsCmd = &cobra.Command{
+	Use:   "events",
+	Short: "Manage events",
+}
+
+func init() {
+	eventsCmd.AddCommand(eventsPushCmd)
+	eventsCmd.AddCommand(eventsListCmd)
+}

cb358ed524cb22f616f722e4557bf6b30f44dbff
patrick-ivann origo <patrickivann.origo@viacesi.fr>
2025-01-23 10:58:31 +0100

    :sparkles: Add list methods for events

---
+++ cmd/events_list.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"obs-pusher/internal/sources"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	eventsListCmd.Flags().String("path", os.Getenv("HOME"), "path of the source xml")
+
+}
+
+var eventsListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List events",
+	Long:  "List events from dictionary",
+	Run: func(cmd *cobra.Command, args []string) {
+		filePath, _ := cmd.Flags().GetString("path")
+
+		notifications, err := sources.ReadEventsList(filePath + "/.obs-pusher/" + "dictionary.xml")
+		if err != nil {
+			println(err.Error())
+			return
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+		fmt.Fprintln(w, "ID\tTEXT")
+		for _, notification := range notifications {
+			fmt.Fprintf(w, "%s\t%s\n",
+				notification.ID, notification.Text)
+		}
+		w.Flush()
+
+	},
+}
